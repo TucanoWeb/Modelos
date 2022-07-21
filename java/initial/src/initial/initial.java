67fafadd182624d6bf7b64f1320aac945c90daba
TucanoWeb <suporte@tucanoweb.com.br>
2022-07-21 12:32:49 -0300

    Initial Java

---
+++ java/initial/src/initial/initial.java
@@ -0,0 +1,19 @@
+package initial;
+
+import initial.model.Cat;
+
+public class initial {
+
+	public static void main(String[] args) {
+		
+		Cat cat = new Cat();
+		
+		System.out.println(cat);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World!" + (a+b));*/
+
+	}
+
+}
